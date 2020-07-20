abd78f62d5e19d4dab90ce40d8e2c25776528d9f
Janeé Peña <janee.pena@gmail.com>
2020-07-20 17:35:00 -0500

    Adding Netlify webhook

--- api/butterfly/models/butterfly.js
+++ api/butterfly/models/butterfly.js
@@ -1,8 +1,29 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
+const axios = require('axios');
 
-module.exports = {};
+module.exports = {
+
+afterCreate: async entry => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+      .catch(() => {
+      });
+  },
+
+
+afterUpdate: async entry => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+      .catch(() => {
+      });
+  },
+
+afterDestroy: async entry => {
+    axios
+      .post(strapi.config.currentEnvironment.staticWebsiteBuildURL, {})
+      .catch(() => {
+        // Ignore
+      });
+  },
+};
---
+++ config/environments/production/custom.js
@@ -0,0 +1,3 @@
+{
+  "staticWebsiteBuildURL": "https://api.netlify.com/build_hooks/5f161ad90c44e00e21c6227d"
+}
